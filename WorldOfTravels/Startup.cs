37a458d2246ac4705263fca029a451c7195fe01b
Omer Anati <omer4554@gmail.com>
2018-09-29 19:27:31 +0300

    Fix missing references of ApplicationUser And fix in _LoginPartial null pointer exception

--- WorldOfTravels/Startup.cs
+++ WorldOfTravels/Startup.cs
@@ -38,7 +38,7 @@ namespace WorldOfTravels
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddDefaultIdentity<ApplicationUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
